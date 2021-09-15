401b9654f0160fe5cd6afe42fbb7f83e37faea3c
Eddie Qiao <eddie.qiao.23@bishops.com>
2021-09-15 12:47:25 -0700

    finds objects

--- find_time.py
+++ find_time.py
@@ -18,3 +18,28 @@ cv2.imshow("Blurred", blurred)
 edged = cv2.Canny(blurred, 30, 150)
 cv2.imshow("Edges", edged)
 cv2.waitKey(0)
+
+(centerX, centerY) = (image.shape[1] // 2, image.shape[0] // 2)
+white = (255, 255, 255)
+cv2.circle(image, (centerX, centerY), 30, white, -1)
+
+(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+print("I count {} things in this image".format(len(cnts)))
+clock = image.copy()
+
+for (i, c) in enumerate(cnts):
+	(x, y, w, h) = cv2.boundingRect(c)
+	
+	print("Thing #{}".format(i + 1))
+	coin = image[y:y + h, x:x + w]
+	cv2.imshow("Coin", coin)
+	cv2.waitKey(0)
+	
+	mask = np.zeros(image.shape[:2], dtype = "uint8")
+	((centerX, centerY), radius) = cv2.minEnclosingCircle(c)
+
+cv2.circle(mask, (int(centerX), int(centerY)), int(radius), 255, -1)
+mask = mask[y:y + h, x:x + w]
+cv2.imshow("Masked Thing", cv2.bitwise_and(coin, coin, mask = mask))
+cv2.waitKey(0)
